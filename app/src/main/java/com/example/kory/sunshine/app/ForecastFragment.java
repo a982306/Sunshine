9b7833f10d6815b77c3ce9f2ab7b681db5d8f045
a982306 <kory.bailey93@yahoo.com>
2015-07-07 20:56:45 -0600

    Added Refresh function

--- app/src/main/java/com/example/kory/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/kory/sunshine/app/ForecastFragment.java
@@ -47,6 +47,8 @@ public class ForecastFragment extends Fragment {
     public  boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -130,6 +132,9 @@ public class ForecastFragment extends Fragment {
                     return null;
                 }
                 forecastJsonStr = buffer.toString();
+
+                Log.v(LOG_TAG, "Forecast JSON String: " + forecastJsonStr);
+
             } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attempting
